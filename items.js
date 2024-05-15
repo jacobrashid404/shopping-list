c869bb55d6683821d33604c08a6ac981989ae00a
Jacob <jacobrashid404@gmail.com>
2024-05-15 16:07:02 -0700

    implemented /items GET and POST routes, GET /:name WIP

--- items.js
+++ items.js
@@ -1,1 +1,30 @@
-const router = 
+import express from "express"
+import { items } from "./fakeDb";
+import { BadRequestError } from "./expressError";
+
+const router = new express.Router();
+
+/** GET /items: gets a list of all items in db */
+router.get("", function(req, res){
+  return res.json({items: items});
+});
+
+router.get("/:name", function(req, res){
+  return res.json()
+});
+
+/** POST /items: accepts JSON: {name: "itemName", price: itemPrice} 
+ * Adds item to items array
+ * Returns JSON: {added: {name: "itemName", price: itemPrice}}
+*/
+router.post("", function(req, res){
+  if (req.body === undefined) throw new BadRequestError();
+  const name = req.body.name;
+  const price = req.body.price;
+  const item = {name, price};
+  items.push(item);
+  
+  return res.json({ added: item});
+});
+
+
