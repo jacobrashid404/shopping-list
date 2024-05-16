c0a789ca9b736798ab4c2698406bb0c84adc2338
Anya Aven <leafeon96@gmail.com>
2024-05-15 20:03:49 -0400

    Fixed any possible bugs

--- items.js
+++ items.js
@@ -15,6 +15,8 @@ router.get("", function (req, res) {
 */
 router.post("", function (req, res) {
   if (req.body === undefined) throw new BadRequestError();
+  if (!(name in req.body) && !(price in req.body)) throw new BadRequestError();
+
   const name = req.body.name;
   const price = req.body.price;
   const item = { name, price };
@@ -26,9 +28,12 @@ router.post("", function (req, res) {
 /** GET /items/:name: get a single item by name in the items "DB"  */
 router.get("/:name", function (req, res) {
   const itemName = req.params.name;
-  
+
   //find index using item name
-  const itemIndex = items.findIndex(item => item.name === itemName)
+  const itemIndex = items.findIndex(item => item.name === itemName);
+
+  if (itemIndex === -1) throw new BadRequestError();
+
   const foundItem = items[itemIndex];
 
   return res.json(foundItem);
@@ -45,17 +50,18 @@ router.patch("/:name", function (req, res) {
 
   const requestedItemName = req.params.name;
 
-  const requestedItem = req.body;
   for (const item of items) {
 
     if (item.name === requestedItemName) {
       //Update item in "DB"
       item.name = req.body.name;
       item.price = req.body.price;
+      return res.json({ updated: item });
     }
   }
 
-  res.json({ updated: items[requestedItem] });
+  //Only get here if we don't have an item with the requested name
+  throw new BadRequestError();
 });
 
 /** DELETE /cats/:name: delete named item from the "DB".
@@ -64,6 +70,7 @@ router.patch("/:name", function (req, res) {
 */
 router.delete("/:name", function (req, res) {
   const requestedItemName = req.params.name;
+
   for (let i = 0; i < items.length; i++) {
     if (items[i].name === requestedItemName) {
       items.splice(i, 1);
