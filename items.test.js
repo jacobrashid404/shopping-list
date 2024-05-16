24b83edc45003cb16d879aeff2cf0c126cf7dde9
Anya Aven <leafeon96@gmail.com>
2024-05-15 20:14:29 -0400

    Added describe organization and not found err

--- items.test.js
+++ items.test.js
@@ -4,16 +4,30 @@ import app from "./app.js";
 
 import { items } from "./fakeDb";
 
-test("/items", async function() {
-  const resp = await request(app).get("/items");
-  expect(resp.text).toEqual(items);
-})
+describe("GET /items ", function () {
 
-test("/items", async function(){
-  const resp = await request(app)
-    .post("/items")
-    .send({name: "testName", price: 1});
-  
-  expect(resp.body).toEqual({name: "testName", price: 1});
-  expect(resp.statusCode).toEqual(201);
-})
+  test("returns all items in 'db'", async function () {
+    const resp = await request(app).get("/items");
+    expect(resp.text).toEqual(items);
+  });
+});
+
+describe("POST /items", function () {
+  test("adding adding an item to the DB", async function () {
+    const resp = await request(app)
+      .post("/items")
+      .send({ name: "testName", price: 1 });
+
+    expect(resp.body).toEqual({ name: "testName", price: 1 });
+    expect(resp.statusCode).toEqual(201);
+  });
+
+  test("Test that we throw an NotFound err when we send incorrect URL params",
+    async function () {
+      const resp = await request(app)
+        .post("/items")
+        .send({ random: "testName", bad: 1 });
+
+      expect(resp.statusCode).toEqual(404);
+    });
+});
